f5e8826dcd78f04739be81be472bd537fe7458a7
tarunuday <tarunuday@gmail.com>
2013-06-26 17:53:05 +0530

    Added shadow

--- index.php
+++ index.php
@@ -10,9 +10,10 @@
 		font-family: "slant";
 		src: url(font/slant.ttf) format("truetype");
 	}
-	#tathva
+	.tathva
 	 {
 		font-family: "tathva";
+		font-size: 565%;
 	 }
 	 .tab
 	 {
@@ -24,52 +25,54 @@
 	<title>Tathva '13</title>
 </head>
 <body>
-	<div class="tab red">
-		<div class="vertical-align-one tab">
-			<div class="vertical-align-two">
-				<div class="vertical-align-three">Competitions</div>
+	<div id="colours">
+		<div class="tab red">
+			<div class="vertical-align-one tab">
+				<div class="vertical-align-two">
+					<div class="vertical-align-three">Competitions</div>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="tab orange">
-		<div class="vertical-align-one tab">
-			<div class="vertical-align-two">
-				<div class="vertical-align-three">Workshops</div>
+		<div class="tab orange">
+			<div class="vertical-align-one tab">
+				<div class="vertical-align-two">
+					<div class="vertical-align-three">Workshops</div>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="tab yellow">
-		<div class="vertical-align-one tab">
-			<div class="vertical-align-two">
-				<div class="vertical-align-three">Exhibitions</div>
+		<div class="tab yellow">
+			<div class="vertical-align-one tab">
+				<div class="vertical-align-two">
+					<div class="vertical-align-three">Exhibitions</div>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="tab green">
-		<div class="vertical-align-one tab">
-			<div class="vertical-align-two">
-				<div class="vertical-align-three">Lectures</div>
+		<div class="tab green">
+			<div class="vertical-align-one tab">
+				<div class="vertical-align-two">
+					<div class="vertical-align-three">Lectures</div>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="tab blue">
-		<div class="vertical-align-one tab">
-			<div class="vertical-align-two">
-				<div class="vertical-align-three">Online</div>
+		<div class="tab blue">
+			<div class="vertical-align-one tab">
+				<div class="vertical-align-two">
+					<div class="vertical-align-three">Online</div>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="tab indigo">
-		<div class="vertical-align-one tab">
-			<div class="vertical-align-two">
-				<div class="vertical-align-three">Nites</div>
+		<div class="tab indigo">
+			<div class="vertical-align-one tab">
+				<div class="vertical-align-two">
+					<div class="vertical-align-three">Nites</div>
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="tab violet">
-		<div class="vertical-align-one tab">
-			<div class="vertical-align-two">
-				<div class="vertical-align-three">Highlights</div>
+		<div class="tab violet">
+			<div class="vertical-align-one tab bottom-shadow ">
+				<div class="vertical-align-two">
+					<div class="vertical-align-three">Highlights</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -77,9 +80,14 @@
 	<div id="main">
 		<div id="main-head">
 			<img id="logo" src="image/tathva-logo.png"/>
-			<div id="title">tathva</div>
+			<div id="title" class="tathva">tathva</div>
 		</div>
 	</div>
-	<div id="footer"></div>
+	<div id="footer">
+		<div id="ticker">
+		</div>
+		<div id="footer-menu">
+		</div>
+	</div>
 </body>
 </html>
